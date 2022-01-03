4206c6cc8a9ee2efbf1d287f37dad0cbe6dda609
Logan Dallalio <logan@dallalioweb.dev>
2022-01-03 04:04:56 -0600

    edit

--- src/context/FacebookContext.js
+++ src/context/FacebookContext.js
@@ -53,6 +53,7 @@ export const FacebookProvider = ({ children }) => {
 			});
 		}, 1000);
 	}, [isLoggedIn]);
+
 	const options = {
 		method: 'GET',
 		url: `https://graph.facebook.com/v12.0/me?fields=albums.fields(photos.fields(source))&access_token=${userToken}`,
